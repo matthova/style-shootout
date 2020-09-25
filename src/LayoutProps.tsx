befa21e31cace13b810fd90b75867152e9eae967
Matt Hova <matthova87@gmail.com>
2020-09-25 08:11:36 -0700

    don't line-break on non-bold text

--- src/LayoutProps.tsx
+++ src/LayoutProps.tsx
@@ -24,6 +24,7 @@ export const LayoutProps: React.FC = () => {
         >
           This form is very important
           <Text
+            as="span"
             typography={{
               mobile: "heading0",
               tablet: "heading2",
